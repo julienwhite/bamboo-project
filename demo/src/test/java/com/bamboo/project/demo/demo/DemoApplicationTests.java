db257a397edc7744fffef3fe3647f8ae4b854c1b
julienwhite <julienwhiteinfo@gmail.com>
2018-10-15 13:36:14 +1100

    demo maven project, run with -DskipTests

---
+++ demo/src/test/java/com/bamboo/project/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.bamboo.project.demo.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.cloud.task.*;
+import org.springframework.cloud.task.configuration.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = {"spring.cloud.task.closecontext_enabled=false"})
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("hello");
+	}
+
+}
