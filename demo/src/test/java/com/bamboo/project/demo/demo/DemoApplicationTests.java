025e8526b389e9d3180f27456c57faeb870df0f3
julienwhite <julienwhiteinfo@gmail.com>
2018-10-16 15:20:06 +1100

    resolved config errors

--- demo/src/test/java/com/bamboo/project/demo/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/bamboo/project/demo/demo/DemoApplicationTests.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.cloud.task.*;
-import org.springframework.cloud.task.configuration.*;
+// import org.springframework.cloud.task.*;
+// import org.springframework.cloud.task.configuration.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(properties = {"spring.cloud.task.closecontext_enabled=false"})
+//@TestPropertySource(properties = {"spring.cloud.task.closecontext_enabled=false"})
 public class DemoApplicationTests {
 
 	@Test
